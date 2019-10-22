d92afcd6aa19ef37651623821f1aff3dc6f90d28
Jason Harris <sirmanlypowers@gmail.com>
2019-10-22 10:58:10 -0700

    Improved the file loading.

--- cmd/emu/main.go
+++ cmd/emu/main.go
@@ -9,7 +9,7 @@
 package main
 
 import (
-	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -29,51 +29,58 @@ type memory struct {
 	ram [64 << 10]uint8
 }
 
+// Read8 reads a byte from memory.
 func (m *memory) Read8(adr uint16) uint8 {
 	return m.ram[adr]
 }
 
+// Write8 writes a byte to memory.
 func (m *memory) Write8(adr uint16, val uint8) {
 	m.ram[adr] = val
 }
 
-func (m *memory) Load(filename string) error {
-
-	x, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return err
-	}
-
-	// validate the header
-	if string(x[0:5]) != "sim65" {
-		return errors.New("bad magic")
-	}
-	if x[5] != 2 {
-		return errors.New("bad version")
-	}
-	if x[6] != 0 {
-		return errors.New("bad cpu")
-	}
-
-	// load the code
-	load := uint16(x[8]) | (uint16(x[9]) << 8)
-	for i, v := range x[12:] {
-		m.Write8(load+uint16(i), v)
-	}
-
-	// setup the reset address
-	m.Write8(cpu.RstAddress+0, x[10])
-	m.Write8(cpu.RstAddress+1, x[11])
-
-	return nil
-}
-
 func newMemory() *memory {
 	m := memory{}
+	// all 0xffs
 	for i := range m.ram {
 		m.ram[i] = 0xff
 	}
 	return &m
+}
+
+// Load loads a sim6502 style executable file (See cc65).
+func (m *memory) Load(filename string) (string, error) {
+
+	// get the file contents
+	x, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	// validate the header
+	if string(x[0:5]) != "sim65" {
+		return "", fmt.Errorf("%s: bad magic", filename)
+	}
+	if x[5] != 2 {
+		return "", fmt.Errorf("%s: bad version", filename)
+	}
+	if x[6] != 0 {
+		return "", fmt.Errorf("%s: bad cpu type", filename)
+	}
+
+	// copy the code to the load address
+	loadAdr := uint16(x[8]) | (uint16(x[9]) << 8)
+	for i, v := range x[12:] {
+		m.Write8(loadAdr+uint16(i), v)
+	}
+	endAdr := loadAdr + uint16(len(x[12:]))
+
+	// setup the reset address
+	rstAdr := uint16(x[10]) | (uint16(x[11]) << 8)
+	m.Write8(cpu.RstAddress+0, x[10])
+	m.Write8(cpu.RstAddress+1, x[11])
+
+	return fmt.Sprintf("%s code %04x-%04x reset %04x", filename, loadAdr, endAdr, rstAdr), nil
 }
 
 //-----------------------------------------------------------------------------
@@ -86,17 +93,18 @@ type userApp struct {
 }
 
 // newUserApp returns a user application.
-func newUserApp() *userApp {
+func newUserApp(fname string) (*userApp, error) {
 	mem := newMemory()
-	err := mem.Load("./test1")
+	status, err := mem.Load(fname)
 	if err != nil {
-		fmt.Printf("%s\n", err)
+		return nil, err
 	}
+	fmt.Printf("%s\n", status)
 	cpu := cpu.New6502(mem)
 	return &userApp{
 		mem: mem,
 		cpu: cpu,
-	}
+	}, nil
 }
 
 // Put outputs a string to the user application.
@@ -107,16 +115,31 @@ func (user *userApp) Put(s string) {
 //-----------------------------------------------------------------------------
 
 func main() {
-	c := cli.NewCLI(newUserApp())
+	// command line flags
+	fname := flag.String("f", "out.bin", "executable file")
+	flag.Parse()
+
+	// create the application
+	app, err := newUserApp(*fname)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+
+	// create the cli
+	c := cli.NewCLI(app)
 	c.HistoryLoad(historyPath)
 	c.SetRoot(menuRoot)
 	c.SetPrompt("emu> ")
+
+	// run the cli
 	for c.Running() {
 		c.Run()
 	}
+
+	// exit
 	c.HistorySave(historyPath)
 	os.Exit(0)
-
 }
 
 //-----------------------------------------------------------------------------
