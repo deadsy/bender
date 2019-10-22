bd31b991f15ff302ea46c4f56364d0ec2c39cee8
Jason Harris <sirmanlypowers@gmail.com>
2019-10-21 17:35:01 -0700

    Changes to the test code.

--- test/test1/main.c
+++ test/test1/main.c
@@ -1,7 +1,18 @@
 
 #include <stdio.h>
+#include <conio.h>
+
+void foo(void) {
+  char tmp[2];
+  int i;
+  tmp[1] = 0;
+  for (i = 0; i < 10; i ++) {
+    tmp[0] = '0' + i;
+    puts(tmp);
+  }
+}
 
 void main(void) {
-  puts("Hello World!\n");
-  while(1);
+  puts("Hello World!");
+  foo();
 }
