7b2975038756f2dc3f79c40a61e3282a83d40f6b
Jason Harris <sirmanlypowers@gmail.com>
2023-05-25 08:08:44 -0700

    Prep for collision detection.

--- test/test4/main.c
+++ test/test4/main.c
@@ -1,81 +1,78 @@
 //-----------------------------------------------------------------------------
+/*
 
-//#include <string.h>
-//#include <ctype.h>
-//#include <6502.h>
-//#include <cbm.h>
+C&N Tetris
 
-#include <stdint.h>
-#include <stdio.h>
+*/
+//-----------------------------------------------------------------------------
+
+//#include <stdio.h>
 #include <conio.h>
-#include <time.h>
+//#include <time.h>
 
 #include <c64.h>
 
+typedef unsigned int u16;
 typedef unsigned char u8;
-typedef char s8;
+typedef signed char s8;
+typedef signed int s16;
 
 //-----------------------------------------------------------------------------
 
 // return the current screen ram location
-uint8_t *get_screenram(void) {
-	return (uint8_t *) ((VIC.addr >> 4) << 10);
+u8 *get_screenram(void) {
+	return (u8 *) ((VIC.addr >> 4) << 10);
 }
 
 #define SCREEN_RAM 0x0400
 #define SCREEN_COLOR 0xD800
 
-static void set_xy(u8 x, u8 y, u8 c, u8 color) {
-	uint8_t *ofs = (uint8_t *) ((40 * y) + x);
-	ofs[SCREEN_RAM] = c;
-	ofs[SCREEN_COLOR] = color;
-}
-
 //-----------------------------------------------------------------------------
 
 typedef s8 xy[2];
-typedef xy layout[4];
+typedef xy layout[3];
 
 #define BG_COLOR COLOR_BLACK
 
-#define P_CHAR 0xa0
+#define P_ON 0xa0
+#define P_OFF 0x20
 
 static const layout p_layout[] = {
 	// I
-	{{0, 0}, {1, 0}, {2, 0}, {-1, 0}},
-	{{0, 0}, {0, 1}, {0, 2}, {0, -1}},
-	{{0, 0}, {-1, 0}, {-2, 0}, {1, 0}},
-	{{0, 0}, {0, -1}, {0, -2}, {0, 1}},
+	{{1, 0}, {2, 0}, {-1, 0}},
+	{{0, 40}, {0, 80}, {0, -40}},
+	{{-1, 0}, {-2, 0}, {1, 0}},
+	{{0, -40}, {0, -80}, {0, 40}},
 	// J
-	{{0, 0}, {-1, 0}, {-1, -1}, {1, 0}},
-	{{0, 0}, {0, -1}, {1, -1}, {0, 1}},
-	{{0, 0}, {1, 0}, {1, 1}, {-1, 0}},
-	{{0, 0}, {0, 1}, {-1, 1}, {0, -1}},
+	{{-1, 0}, {-1, -40}, {1, 0}},
+	{{0, -40}, {1, -40}, {0, 40}},
+	{{1, 0}, {1, 40}, {-1, 0}},
+	{{0, 40}, {-1, 40}, {0, -40}},
 	// L
-	{{0, 0}, {1, 0}, {1, -1}, {-1, 0}},
-	{{0, 0}, {0, 1}, {1, 1}, {0, -1}},
-	{{0, 0}, {-1, 0}, {-1, 1}, {1, 0}},
-	{{0, 0}, {0, -1}, {-1, -1}, {0, 1}},
+	{{1, 0}, {1, -40}, {-1, 0}},
+	{{0, 40}, {1, 40}, {0, -40}},
+	{{-1, 0}, {-1, 40}, {1, 0}},
+	{{0, -40}, {-1, -40}, {0, 40}},
 	// S
-	{{0, 0}, {0, -1}, {1, -1}, {-1, 0}},
-	{{0, 0}, {1, 0}, {1, 1}, {0, -1}},
-	{{0, 0}, {0, 1}, {-1, 1}, {1, 0}},
-	{{0, 0}, {-1, 0}, {-1, -1}, {0, 1}},
+	{{0, -40}, {1, -40}, {-1, 0}},
+	{{1, 0}, {1, 40}, {0, -40}},
+	{{0, 40}, {-1, 40}, {1, 0}},
+	{{-1, 0}, {-1, -40}, {0, 40}},
 	// Z
-	{{0, 0}, {0, -1}, {-1, -1}, {1, 0}},
-	{{0, 0}, {1, 0}, {1, -1}, {0, 1}},
-	{{0, 0}, {0, 1}, {1, 1}, {-1, 0}},
-	{{0, 0}, {-1, 0}, {-1, 1}, {0, -1}},
+	{{0, -40}, {-1, -40}, {1, 0}},
+	{{1, 0}, {1, -40}, {0, 40}},
+	{{0, 40}, {1, 40}, {-1, 0}},
+	{{-1, 0}, {-1, 40}, {0, -40}},
 	// T
-	{{0, 0}, {0, -1}, {-1, 0}, {1, 0}},
-	{{0, 0}, {1, 0}, {0, -1}, {0, 1}},
-	{{0, 0}, {0, 1}, {1, 0}, {-1, 0}},
-	{{0, 0}, {-1, 0}, {0, 1}, {0, -1}},
+	{{0, -40}, {-1, 0}, {1, 0}},
+	{{1, 0}, {0, -40}, {0, 40}},
+	{{0, 40}, {1, 0}, {-1, 0}},
+	{{-1, 0}, {0, 40}, {0, -40}},
 	// O
-	{{0, 0}, {0, -1}, {1, -1}, {1, 0}},
-	{{0, 0}, {0, -1}, {1, -1}, {1, 0}},
-	{{0, 0}, {0, -1}, {1, -1}, {1, 0}},
-	{{0, 0}, {0, -1}, {1, -1}, {1, 0}},
+	{{0, -40}, {1, -40}, {1, 0}},
+	{{0, -40}, {1, -40}, {1, 0}},
+	{{0, -40}, {1, -40}, {1, 0}},
+	{{0, -40}, {1, -40}, {1, 0}},
 };
 
 static const u8 p_color[] = {
@@ -88,24 +85,31 @@ static const u8 p_color[] = {
 	COLOR_YELLOW,		// O
 };
 
-static void piece(u8 x, u8 y, u8 color, const layout * p) {
-	int i;
-	for (i = 0; i < 4; i++) {
-		set_xy(x + (*p)[i][0], y + (*p)[i][1], P_CHAR, color);
-	}
+static void set(u16 addr, u8 c, u8 color) {
+	u8 *ofs = (u8 *) addr;
+	ofs[SCREEN_RAM] = c;
+	ofs[SCREEN_COLOR] = color;
+}
+
+static void piece(u8 x, u8 y, u8 c, u8 color, const layout * p) {
+	s16 base = (40 * y) + x;
+	set(base, c, color);
+	set(base + (*p)[0][0] + (*p)[0][1], c, color);
+	set(base + (*p)[1][0] + (*p)[1][1], c, color);
+	set(base + (*p)[2][0] + (*p)[2][1], c, color);
 }
 
 static void piece_on(u8 id, u8 x, u8 y, u8 dirn) {
-	piece(x, y, p_color[id], &p_layout[(id << 2) + (dirn & 3)]);
+	piece(x, y, P_ON, p_color[id], &p_layout[(id << 2) + (dirn & 3)]);
 }
 
 static void piece_off(u8 id, u8 x, u8 y, u8 dirn) {
-	piece(x, y, BG_COLOR, &p_layout[(id << 2) + (dirn & 3)]);
+	piece(x, y, P_OFF, BG_COLOR, &p_layout[(id << 2) + (dirn & 3)]);
 }
 
 static void delay(void) {
 	volatile int i;
-	for (i = 0; i < 3000; i++) {
+	for (i = 0; i < 6000; i++) {
 	}
 }
 
